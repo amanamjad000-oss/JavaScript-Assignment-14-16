bdb4e8de70d944d9c29f90934c193945b893513c
amanamjad000-oss <amanamjad000@gmail.com>
2026-01-09 10:57:06 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//Chapter 14-16 (Q No 01)
+//var studentNames = [];
+//console.log("Student Names Array:", studentNames);
+
+//Chapter 14-16 (Q No 02)
+//var studentNames = new Array();
+//console.log("studentNames Array:",studentNames);
+
+//Chapter 14-16 (Q No 03)
+//var fruits =["strawberry","bluebarry","grapes"];
+//console.log("fruits Array:",fruits);
+
+//Chapter 14-16 (Q No 04)
+//var numbers = [40,10,90,30];
+//console.log("Numders Array:",numbers);
+
+//Chapter 14-16 (Q No 05)
+//var boolean = [true,false,true,false];
+//console.log("Boolean Array:",boolean);
+
+//Chapter 14-16 (Q No 06)
+//var mixedArray = ["Pakeeza",30,true,"pink"];
+//console.log("Mixed Array",mixedArray);
+
+//Chapter 14-16 (Q No 07)
+//var qyalification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+//for (var i = 0; i < qualifications.length; i++) {
+//console.log((i + 1) + ") " + qualifications[i]);}
+
+//Chapter 14-16 (Q NO 08)
+//var studentNames = ["jhon","smith","elia"];
+//var studentScores =[450,400,350];
+//var totalMarks = 500;
+//for (var i = 0; i < studentNames.length; i++) {
+//var percentage = (studentScores[i] / totalMarks) * 100;
+//console.log("Score of " + studentNames[i] + " is " + studentScores[i] + ". Percentage: " + percentage + "%");}
+
+//Chapter 14-16 (Q No 09)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Chapter 14-16 (Q No 10)
+//var studentScores = [320,230,480,120];
+//studentScores.reverse();
+//console.log(studentScores);
+
+//Chapter 14-16 (Q No 11)
+//var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+//var selectedCities =cities.slice (2,4);
+//document.write("Cities list: " + "<br>" + cities + "<br><br>");
+//console.log("selectedCities Array",selectedCities);
+
+//Chapter 14-16 (Q No 12)
+var username = ["This","is","my","cat"];
+console.log(username);
+console.log(username.join(""));
+
+
+
+
